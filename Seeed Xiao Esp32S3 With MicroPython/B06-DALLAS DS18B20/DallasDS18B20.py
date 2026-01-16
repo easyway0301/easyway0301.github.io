266dfb9e50fc3ee831128296e594d0d282aada07
easyway0301 <easyway0301@gmail.com>
2026-01-16 20:49:39 +0800

    Create DallasDS18B20.py

---
+++ Seeed Xiao Esp32S3 With MicroPython/B06-DALLAS DS18B20/DallasDS18B20.py
@@ -0,0 +1,21 @@
+from machine import Pin
+import onewire, ds18x20
+import time
+
+dat = Pin(9, Pin.IN, Pin.PULL_UP)  # 建議腳位
+ow = onewire.OneWire(dat)
+ds = ds18x20.DS18X20(ow)
+
+roms = ds.scan()
+print("找到 DS18B20:", roms)
+
+if not roms:
+    print("❌ 沒有偵測到 DS18B20，請檢查接線與 4.7kΩ 上拉電阻")
+else:
+    ds.convert_temp()
+    time.sleep_ms(750)
+
+    for rom in roms:
+        temp = ds.read_temp(rom)
+        print("🌡 溫度:", temp, "°C")
+
